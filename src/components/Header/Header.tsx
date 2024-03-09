861a1d497587242c8c8a2127ea44c4a4c7e293ec
AFKLenb <markermma@gmail.com>
2024-03-09 12:55:16 +0300

    Сделил меню

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,26 @@
+"use client";
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../Navbar/Navbar'
 import { FaBars } from 'react-icons/fa6'
+import NavbarMobile from '../NavbarMobile/NavbarMobile'
 
 const Header = () => {
+
+  const [ openNav, setOpenNav] = useState(false);
+
   return (
-    <header className=' w-full h-[70px] items-center border-b border-zinc-800 bg-zinc-950 '>
-      <div className="container mx-auto">
-        <div className="w-full flex items-center justify-between ">
+    <header className=' w-full h-[70px]  border-b border-zinc-800 bg-zinc-950 '>
+      <div className="container mx-auto flex items-center h-full">
+        <div className="w-full h-full relative flex items-center justify-between ">
           <Link href="/" className=' w-[100px] h-[50px]'>
             <Image className='w-full h-full rounded-md bg-white' src='/assets/image/logo.png' width={200} height={50} alt='Логотип сайта'  />
           </Link>
+          <NavbarMobile containerStyle={` ${openNav ? 'max-h-max border-b border-zinc-800 p-[15px]' : 'max-h-0 overflow-hidden py-0 px-[15px] border-zinc-800/0'} fixed w-full left-0 right-0 top-[71px] transition-all duration-400 bg-zinc-900 md:hidden`}/>
           <Navbar  containerStyle='hidden md:flex' />
           <div className="">
-            <button className=' text-white text-xl w-[45px] h-[45px] flex items-center justify-center border border-zinc-500 rounded-md md:hidden'>
+            <button onClick={ () => setOpenNav(!openNav)} className=' text-white text-xl w-[45px] h-[45px] flex items-center justify-center border border-zinc-500 rounded-md md:hidden'>
               <FaBars/>
             </button>
           </div>
--- src/components/NavbarMobile/NavbarMobile.tsx
+++ src/components/NavbarMobile/NavbarMobile.tsx
@@ -1,10 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
 
-const NavbarMobile = () => {
+// menuData
+const menuLinks = [
+    { label: 'Главная', url: '/' },
+    { label: 'О нас', url: '/about' },
+    { label: 'Акции', url: '/sales' },
+    { label: 'Каталог', url: '/catalog' },
+    { label: 'Контакты', url: '/contacts' },
+]
+
+const NavbarMobile = ( { containerStyle} : { containerStyle : string}) => {
   return (
-    <div>
-      
-    </div>
+    <nav className={`${containerStyle}`}>
+       <menu className='w-full flex flex-col gap-4 '>
+            {menuLinks.map(( item, index) =>{
+                return(
+                    <>
+                        <li key={index}>
+                            <Link className=' text-white uppercase hover:text-accent transition-all duration-300' href={item.url}>{item.label}</Link>
+                        </li>
+                    </>
+                )
+            })}
+        </menu>
+    </nav>
   )
 }
 

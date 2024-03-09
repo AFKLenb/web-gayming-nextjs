4aaef0ac63a1398204fb3c4d166e68b5abc2eb20
AFKLenb <markermma@gmail.com>
2024-03-09 12:33:22 +0300

    Сделали header и navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { FaBars } from 'react-icons/fa6'
 
 const Header = () => {
   return (
-    <header>
-      header
+    <header className=' w-full h-[70px] items-center border-b border-zinc-800 bg-zinc-950 '>
+      <div className="container mx-auto">
+        <div className="w-full flex items-center justify-between ">
+          <Link href="/" className=' w-[100px] h-[50px]'>
+            <Image className='w-full h-full rounded-md bg-white' src='/assets/image/logo.png' width={200} height={50} alt='Логотип сайта'  />
+          </Link>
+          <Navbar  containerStyle='hidden md:flex' />
+          <div className="">
+            <button className=' text-white text-xl w-[45px] h-[45px] flex items-center justify-center border border-zinc-500 rounded-md md:hidden'>
+              <FaBars/>
+            </button>
+          </div>
+        </div>
+      </div>
     </header>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 // menuData
@@ -12,7 +13,17 @@ const menuLinks = [
 const Navbar = ( { containerStyle} : {containerStyle: string} ) => {
   return (
     <nav className={`${containerStyle}`}>
-        <menu className='w-full flex items-center justify-center gap-4 '></menu>
+        <menu className='w-full flex items-center justify-center gap-4 '>
+            {menuLinks.map(( item, index) =>{
+                return(
+                    <>
+                        <li key={index}>
+                            <Link className=' text-white uppercase hover:text-accent transition-all duration-300' href={item.url}>{item.label}</Link>
+                        </li>
+                    </>
+                )
+            })}
+        </menu>
     </nav>
   )
 }
